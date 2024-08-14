5b79d0246fe036df474a10659c9b94360aa53be0
DulanjiAbeysekara <dulanjiabeysekara09@gmail.com>
2024-08-14 19:47:24 +0530

    creating header

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './header.css'
+
+export default function Header() {
+  return (
+    <div className='navBar'>
+
+      
+     <ul>
+     <h1 className='title'>The Heritage Gill</h1>
+        <li>Home</li>
+        <li>About</li>
+        <li>Menu</li>
+        <li>Contact</li>
+
+
+     </ul>
+    </div>
+  )
+}
