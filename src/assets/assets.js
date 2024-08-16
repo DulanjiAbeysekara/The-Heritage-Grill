33bef949190a03fdea120e93d0eb3e16f279a379
DulanjiAbeysekara <dulanjiabeysekara09@gmail.com>
2024-08-16 15:43:55 +0530

    creating list in assets.js

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,7 +1,15 @@
 import logo from './logo.png';
 import cart from './shopping-cart.png';
 import search from './search (4).png';
-import homepage from './home.jpeg'
+import homepage from './home.jpeg';
+
+import menu1 from './menu_1.jpeg';
+import menu2 from './menu_2.jpeg';
+import menu3 from './menu_3.jpeg';
+import menu4 from './menu_4.jpeg';
+import menu5 from './menu_5.jpeg';
+import menu6 from './menu_6.jpeg';
+import menu7 from './menu_7.jpeg';
 
 export const assets = {
     logo,
@@ -9,3 +17,34 @@ export const assets = {
     search,
     homepage
 };
+
+export const menu_list = [
+    {
+        menu_name: "Burger",
+        menu_image: menu1
+    },
+    {
+        menu_name: "Pizza",
+        menu_image: menu2
+    },
+    {
+        menu_name: "Noddle",
+        menu_image: menu3
+    },
+    {
+        menu_name: "Noddle",
+        menu_image: menu4
+    },
+    {
+        menu_name: "Deserts",
+        menu_image: menu5
+    },
+    {
+        menu_name: "Sandwich",
+        menu_image: menu6
+    },
+    {
+        menu_name: "Salad",
+        menu_image: menu7
+    }
+];
---
+++ src/components/ExploerMenu/ExploerMenu.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './ExploerMenu.css'
+
+const ExploerMenu = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ExploerMenu
